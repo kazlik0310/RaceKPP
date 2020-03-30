71aa0e92afcd22503a46f836fee31a871fc455d1
kazlik0310 <62237107+kazlik0310@users.noreply.github.com>
2020-03-30 10:01:51 +0300

    Add files via upload

---
+++ Race/src/com/company/Player.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+
+public class Player {
+
+    public static final int MAX_SPEED=50;
+    public static final int MAX_TOP=10;
+    public static final int MAX_BOTTON=300;
+
+
+    Image img = new ImageIcon("D:\\kazlik\\proga\\KPP\\Race\\src\\player.png").getImage();
+
+    int v=0;
+    int dv=0;
+    int s=0;
+
+    int x=100;
+    int y=30;
+    int dy=0;
+
+    int layer1=0;
+    int layer2=1300;
+
+    public void move(){
+        s+=v;
+        v+=dv;
+        if(v<=0){
+            v=0;
+        }
+        if(v>=MAX_SPEED){
+            v=MAX_SPEED;
+        }
+        y+=dy;
+        if(y<=MAX_TOP){
+            y=MAX_TOP;
+        }
+        if(y>=MAX_BOTTON){
+            y=MAX_BOTTON;
+        }
+        if(layer2 - v <= 0){
+            layer1=0;
+            layer2=1300;
+        }
+        else {
+            layer1 -= v;
+            layer2 -= v;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_RIGHT) {
+            dv=1;
+        }
+        if(key == KeyEvent.VK_LEFT) {
+            dv=-1;
+        }
+        if(key == KeyEvent.VK_UP) {
+            dy=-5;
+        }
+        if(key == KeyEvent.VK_DOWN) {
+            dy=5;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if(key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_LEFT) {
+            dv = 0;
+        }
+        if(key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+
+
+
+}
--- Race/src/com/company/Road.java
+++ Race/src/com/company/Road.java
@@ -3,17 +3,48 @@ package com.company;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Road extends JPanel {
+public class Road extends JPanel implements ActionListener {
+
+    Timer mainTimer = new Timer(20, this);
 
     Image img = new ImageIcon("D:\\kazlik\\proga\\KPP\\Race\\src\\road.jpg").getImage();
 
+    Player p = new Player();
+
+    public Road(){
+        mainTimer.start();
+        addKeyListener(new myKeyAdapter());
+        setFocusable(true);
+    }
+
+    private class myKeyAdapter extends KeyAdapter {
+        public void keyPressed (KeyEvent e){
+            p.keyPressed(e);
+        }
+        public void keyReleased(KeyEvent e){
+            p.keyReleased(e);
+        }
+    }
+
     public void paint(Graphics g){
         g = (Graphics2D) g;
-        g.drawImage(img, 0, 0, null);
+        g.drawImage(img, p.layer1, 0, null);
+        g.drawImage(img, p.layer2, 0, null);
+        g.drawImage(p.img, p.x, p.y, null);
+    }
+
+
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
+        p.move();
+        repaint();
     }
 }
